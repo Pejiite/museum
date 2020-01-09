974d7ea0193483bd2bbf18be03e779c6798c64bc
debian <Tristan Guillemot>
2020-01-09 14:59:36 +0100

    yo

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :arts
-  root to: "arts#index" # redirige vers arts
+  root to: "pages#arts" # redirige vers arts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
